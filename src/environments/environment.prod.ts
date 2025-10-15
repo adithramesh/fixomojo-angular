9f0d0dc9d9b79fea67b0137e0c5689fed654ac03
adithramesh <adithramesh90@gmail.com>
2025-10-15 23:23:45 +0530

    add prod keys

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,7 @@
 export const environment = {
   production: true,
+  LOCATION_IQ_API_KEY:'pk.06165ee01186e3b07b09c63eac711fbb',
+  STREAM_API_KEY:"qzs3h5vemeg5",
   BACK_END_API_URL: 'https://api.fixomojo.homes',
   CLOUDINARY_CLOUD_NAME: 'dj8j4pwqk'
 };
